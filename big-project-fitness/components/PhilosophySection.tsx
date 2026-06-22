e5bbc5ef9eb7a5e3b7ce556667e36a560f604e4e
Pree <manchikantipreetham@gmail.com>
2026-06-22 14:55:17 +0530

    deployment fixes

--- big-project-fitness/components/PhilosophySection.tsx
+++ big-project-fitness/components/PhilosophySection.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, useScroll, useTransform, MotionValue } from "framer-motion";
 import { useRef } from "react";
 
 export function PhilosophySection() {
@@ -42,7 +42,7 @@ export function PhilosophySection() {
   );
 }
 
-function Word({ word, progress, range }: { word: string, progress: any, range: number[] }) {
+function Word({ word, progress, range }: { word: string, progress: MotionValue<number>, range: number[] }) {
   const opacity = useTransform(progress, range, [0.15, 1]);
   return (
     <motion.span style={{ opacity }} className="inline-block uppercase">
